a6d8b9a25fec388eef4afd84c7b5db491fe9ee1c
alice19-meet <alice19@meet.mit.edu>
2018-08-07 23:01:25 +0300

    flask

--- exercises/app.py
+++ exercises/app.py
@@ -4,9 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     players=["Federer","Williams","Nadal"]
+    likes_same_sport=True
     return render_template(
         "index.html",
-        players=players)
+        players=players,
+        likes_same_sport=likes_same_sport)
 
 if __name__ == '__main__':
    app.run(debug = True)
